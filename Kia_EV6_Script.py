6aa9f05bc3ac7a8cba4165697cdad4cf549b3630
Lazgar <34341913+Lazgar@users.noreply.github.com>
2025-09-03 10:15:59 +0200

    Update Kia_EV6_Script.py

--- Kia_EV6_Script.py
+++ Kia_EV6_Script.py
@@ -207,6 +207,7 @@ def get_full_status():
         client.publish(mqttbasetopic + "location_speed", ret.rstrip("'"))
     else:
       client.publish(mqttbasetopic + searchValue.rstrip("='"), ret.rstrip("'"))
+    client.publish(mqttbasetopic + "manufacturer", apibrand)
 
 try:
    client = mqtt.Client(mqttclientid)
