14481807ba188b5c7b586085058ae5be35880ed1
Lazgar <34341913+Lazgar@users.noreply.github.com>
2026-05-19 21:34:29 +0200

    Update Kia_EV6_Script_v2.0.py

--- Kia_EV6_Script_v2.0.py
+++ Kia_EV6_Script_v2.0.py
@@ -184,8 +184,9 @@ def on_message(client, userdata, msg):
             logging.info("MQTT Befehl empfangen: Start Klima")
             client.publish(f"{mqtt_topic}command", "pending", retain=True)
             try:
+                climateClass = ClimateRequestOptions(**json.loads(msg.payload))
                 # API liefert das Action-Objekt oder die ID zurueck
-                action_response = vm.start_climate(vehicle_id)
+                action_response = vm.start_climate(vehicle_id,climateClass)
                 client.publish(f"{mqtt_topic}response", action_response, retain=True)
                 # Extrahiere die ID (je nach API-Format z.B. direkt oder als Attribut)
                 action_id = getattr(action_response, 'action_id', action_response)
